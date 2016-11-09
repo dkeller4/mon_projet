43e4c2d8bda2c415a83fed0b1fa70e69b261e9cf
dimitrikeller <keller.dimitri@gmail.com>
2016-11-09 15:51:56 -0500

    Layout HTML CSS les cases du tableau

--- script/main.js
+++ script/main.js
@@ -5,12 +5,14 @@ var nombre_rangees = 14;
 var nombre_colonnes = 20;
 var i,j ;
 
-for (i=1; i<=nombre_rangees ; i++) {
-    for (j=1; i<=nombre_colonnes ; j++){
-        var text= " '<div id=r' + i + 'c' + j + '></div>' ";
-        document.getElementById("gamebox").innerHTML=+ text ;
-    }
-
+function Dessinertableau() {
+    for (i=1; i<=nombre_rangees ; i++) {
+        for (j = 1; j <= nombre_colonnes; j++) {
+            var text = "<div id=r" + i + "c" + j + "></div>";
+            document.getElementById("gamebox").innerHTML += text;
         }
+    }
+}
 
-console.log(text);
+Dessinertableau();
+
