b8cbcf3ab81d8279cc6c65c2ace8c000f87b7785
dimitrikeller <keller.dimitri@gmail.com>
2016-11-09 15:10:00 -0500

    Layout HTML CSS les cases du tableau

--- script/main.js
+++ script/main.js
@@ -1,7 +1,16 @@
 /*** Created by dkeller on 2016-10-27.*/
 "use strict"; /* Exiger la déclaration */
 
-var gameboxhtml=document.getElementById("gamebox").getElementsByTagName("table");
-console.log(gameboxhtml.outerhtml);
+var nombre_rangees = 14;
+var nombre_colonnes = 20;
+var i,j ;
 
+for (i=1; i<=nombre_rangees ; i++) {
+    for (j=1; i<=nombre_colonnes ; j++){
+        var text= " '<div id=r' + i + 'c' + j + '></div>' ";
+        document.getElementById("gamebox").innerHTML=+ text ;
+    }
 
+        }
+
+console.log(text);
