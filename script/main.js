92fc2384f266b647733d97cd882f479bb3b2c422
dimitrikeller <keller.dimitri@gmail.com>
2016-11-09 16:41:16 -0500

    Layout HTML CSS Dessiner tuiles

--- script/main.js
+++ script/main.js
@@ -5,14 +5,21 @@ var nombre_rangees = 14;
 var nombre_colonnes = 20;
 var i,j ;
 
-function Dessinertableau() {
+function Inittableau() {
     for (i=1; i<=nombre_rangees ; i++) {
         for (j = 1; j <= nombre_colonnes; j++) {
-            var text = "<div id=r" + i + "c" + j + "></div>";
+            var text = "<div id=r" + i + "c" + j + ' class="img1"></div>';
             document.getElementById("gamebox").innerHTML += text;
         }
     }
 }
 
-Dessinertableau();
+function Dessinerimg (positionx,positiony, numero_img) { //position x pour les rangées, position y pour les colonnes
+    var div= document.getElementById("r"+positionx+"c"+positiony);
+    div.outerHTML=div.outerHTML.replace("img1","img" + numero_img);
+}
+
+Inittableau();
+Dessinerimg (10,20, 0);
+Dessinerimg (11,20, 0);
 
