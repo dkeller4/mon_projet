ae62d67eae4005fb6783d90820e52b6d20a1a4e9
dimitrikeller <keller.dimitri@gmail.com>
2016-11-15 22:26:31 -0500

    Le mouvement

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 /*** Created by dkeller on 2016-10-27.*/
 "use strict"; /* Exiger la déclaration */
-var pos = {
-    x:0,
-    y:0
-}
+var pos_pacman = {
+    x:1,
+    y:1
+};
 var nombre_rangees = 14;
 var nombre_colonnes = 20;
 var i,j ;
@@ -45,46 +45,115 @@ function Dessinermap (map) { // la map introduite doit être un tableau à deux 
         }
     }
 }
-function Dessinerpacman(positionx,positiony){
-    document.getElementById("gamebox").innerHTML += "<div class='pacman' data-position=r"+positionx+"\c"+positiony+'></div>';
-    var increase_x = largeur_div*(positionx-1);
-    var increase_y = hauteur_div *(positiony-1);
-    $(".pacman").css("left" , increase_x);
-    $(".pacman").css("top" , increase_y);
-    return document.getElementsByClassName("pacman")[0];
+
+function Dessinerpacman(position){
+    $('#pacman').remove();
+
+    document.getElementById("gamebox").innerHTML += "<div id='pacman' ></div>";
+    var $pacman =$('#pacman');
+    var increase_x = largeur_div*(position.x-1);
+    var increase_y = hauteur_div *(position.y-1);
+    $pacman.css("left" , increase_x);
+    $pacman.css("top" , increase_y);
 }
 
-function movedown(object) {
-    var top = parseFloat(object.style.top);
-    var top_max = parseFloat(object.style.top)+hauteur_div; // Le problème est ici !!!
-    console.log(object.style.top);
-    console.log(top_max);
-    console.log('On arrete');
+function movedown(position) {
+    var from_top = parseFloat(object.style.top);
+    var to_top = parseFloat(object.style.top)+hauteur_div; // Le problème est ici !!!
+
     var id = setInterval(frame, 10);
     function frame() {
-        if (parseFloat(object.style.top) > top_max) {
+        if (from_top > to_top) {
             clearInterval(id);
             id=null;
         } else {
-            top++;
-            object.style.top = top + 'px';
+            from_top++;
+            object.style.top = from_top + 'px';
         }
     }
 }
+
+function vers_le_bas() {
+    var to_top = pos_pacman.y+1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos_pacman.y == to_top) {
+            clearInterval(id);
+            id=null;
+        } else {
+            pos_pacman.y+=0.25;
+            Dessinerpacman(pos_pacman);
+        }
+    }
+}
+
+function vers_le_haut() {
+    var to_top = pos_pacman.y-1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos_pacman.y == to_top) {
+            clearInterval(id);
+            id=null;
+        } else {
+            pos_pacman.y-=0.25;
+            Dessinerpacman(pos_pacman);
+        }
+    }
+}
+function vers_la_gauche() {
+    var to_top = pos_pacman.x-1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos_pacman.x == to_top) {
+            clearInterval(id);
+            id=null;
+        } else {
+            pos_pacman.x-=0.25;
+            Dessinerpacman(pos_pacman);
+        }
+    }
+}
+function vers_la_droite() {
+    var to_top = pos_pacman.x+1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos_pacman.x == to_top) {
+            clearInterval(id);
+            id=null;
+        } else {
+            pos_pacman.x+=0.25;
+            Dessinerpacman(pos_pacman);
+        }
+    }
+}
+function Mouvement(e) {
+
+    e = e || window.event;
+    if (e.keyCode == '38') {
+        vers_le_haut();
+    }
+    else if (e.keyCode == '40') {
+        vers_le_bas();
+    }
+    else if (e.keyCode == '37') {
+        vers_la_gauche();
+    }
+    else if (e.keyCode == '39') {
+        vers_la_droite();
+    }
+}
+
+
 // Le code débute ici !!!
-var id
 Dessinermap(map_niveau1);
 var div_info = $("#r1c1");
 var largeur_div = div_info.width();
 var hauteur_div = div_info.height();
 
-var pacman = Dessinerpacman(13,3);
-
-addEventListener("click",function(){
-    movedown(pacman);
-});
 
 
+Dessinerpacman(pos_pacman);
+document.onkeydown = Mouvement;
 
 
 
@@ -94,6 +163,8 @@ addEventListener("click",function(){
 $("label").click(function(){
     $(this).slideUp();
 });
+
+
 
 
 
