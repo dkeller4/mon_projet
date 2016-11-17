b3321c288eebb548333787784fa64fbb560a3850
dimitrikeller <keller.dimitri@gmail.com>
2016-11-17 17:00:29 -0500

    Collision avec les murs + Dessiner érables aléatoirement

--- script/main.js
+++ script/main.js
@@ -1,12 +1,14 @@
 /*** Created by dkeller on 2016-10-27.*/
 "use strict"; /* Exiger la déclaration */
 var pos_pacman = {
-    x:1,
-    y:1
+    x:0,
+    y:0
 };
+const nombre_erables1 = 10;
 var nombre_rangees = 14;
 var nombre_colonnes = 20;
-var i,j ;
+var i,j
+var destroyed =0 ;
 var map_niveau1 = [
     [0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1],
     [0,0,0,0,1,1,1,1,0,1,1,1,1,0,1,1,0,0,0,0],
@@ -51,10 +53,31 @@ function Dessinerpacman(position){
 
     document.getElementById("gamebox").innerHTML += "<div id='pacman' ></div>";
     var $pacman =$('#pacman');
-    var increase_x = largeur_div*(position.x-1);
-    var increase_y = hauteur_div *(position.y-1);
+    var increase_x = largeur_div*(position.x);
+    var increase_y = hauteur_div *(position.y);
     $pacman.css("left" , increase_x);
     $pacman.css("top" , increase_y);
+}
+function Dessinererable(nombre){
+    $('.erable').remove();
+    var data_erables = [];
+
+
+    for (i=0 ; i< nombre ; i++) {
+
+        do {
+            var random_x =(Math.floor(Math.random()*20));
+            var random_y =(Math.floor(Math.random()*14));
+        } while (map_niveau1[random_y][random_x] == 1 || (random_x ==0 && random_y==0) ); // Pour la map les x et les y sont inversés
+
+        document.getElementById("gamebox").innerHTML += "<div class='erable' id='" + i + "' ></div>";
+        var erable = $('.erable#'+i);
+        erable.css("left" , largeur_div*(random_x));
+        erable.css("top" , hauteur_div *(random_y));
+        data_erables[i].x=random_x;
+        data_erables[i].y=random_y;
+    }
+      return data_erables;
 }
 
 function movedown(position) {
@@ -129,18 +152,21 @@ function vers_la_droite() {
 function Mouvement(e) {
 
     e = e || window.event;
-    if (e.keyCode == '38') {
+    if (e.keyCode == '38' && map_niveau1[pos_pacman.y-1][pos_pacman.x] != 1) { // Pour la map les x et les y sont inversés
         vers_le_haut();
     }
-    else if (e.keyCode == '40') {
+    else if (e.keyCode == '40' && map_niveau1[pos_pacman.y+1][pos_pacman.x] != 1) {
         vers_le_bas();
     }
-    else if (e.keyCode == '37') {
+    else if (e.keyCode == '37' && map_niveau1[pos_pacman.y][pos_pacman.x-1]!= 1) {
         vers_la_gauche();
     }
-    else if (e.keyCode == '39') {
+    else if (e.keyCode == '39' && map_niveau1[pos_pacman.y][pos_pacman.x+1] !=1) {
         vers_la_droite();
     }
+   // for (i=0 ; i<nombre_erables1-destroyed ; i++){
+      // if(pos_pacman == data_erables[i] {
+       //}
 }
 
 
@@ -153,11 +179,11 @@ var hauteur_div = div_info.height();
 
 
 Dessinerpacman(pos_pacman);
+var data_erables = Dessinererable(nombre_erables1);
 document.onkeydown = Mouvement;
 
 
-
-
+console.log(data_erables); // Je n'arrive pas à accéder à l'information en dehors de la fonction
 
 
 $("label").click(function(){
