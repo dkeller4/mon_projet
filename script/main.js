041b57945581a809f9dc2414bea1718559e68839
dimitrikeller <keller.dimitri@gmail.com>
2016-11-21 17:45:42 -0500

    High Score

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ var pos_pacman = {
 const NOMBRE_ERABLES1 = 30;
 var nombre_rangees = 14;
 var nombre_colonnes = 20;
-var i,j,random_x,random_y;
+var i,j,random_x,random_y,t;
 var s = 0;
 var m = 0;
 var destroyed =0 ;
@@ -100,7 +100,6 @@ function random_x_y(){
         random_x = (Math.floor(Math.random() * 20));
         random_y = (Math.floor(Math.random() * 14));
     } while (map_niveau1[random_y][random_x] == 1 || (random_x == 0 && random_y == 0) || double_erable()==true);
-
 }
 
     /**
@@ -172,42 +171,45 @@ function vers_la_droite() {
     }
 }
 function Mouvement(e) {
-
     e = e || window.event;
     if (e.keyCode == '38' && map_niveau1[pos_pacman.y-1][pos_pacman.x] == 0) { // Pour la map les x et les y sont inversés
+        pacman_vs_erables(pos_pacman.x,pos_pacman.y-1);
         vers_le_haut();
     }
     else if (e.keyCode == '40' && map_niveau1[pos_pacman.y+1][pos_pacman.x] == 0) {
+        pacman_vs_erables(pos_pacman.x,pos_pacman.y+1);
         vers_le_bas();
     }
     else if (e.keyCode == '37' && map_niveau1[pos_pacman.y][pos_pacman.x-1] == 0) {
+        pacman_vs_erables(pos_pacman.x-1,pos_pacman.y);
         vers_la_gauche();
     }
     else if (e.keyCode == '39' && map_niveau1[pos_pacman.y][pos_pacman.x+1] == 0) {
+        pacman_vs_erables(pos_pacman.x+1,pos_pacman.y);
         vers_la_droite();
     }
-    pacman_vs_erables();
 }
-
 /**
  * Cette fonction compare la position du pacman avec les positions des érables
  * @constructor
  */
-function pacman_vs_erables() {
-    for (i=0 ; i<NOMBRE_ERABLES1-destroyed ; i++){
-        if(pos_pacman.x == data_erables[i].x && pos_pacman.y == data_erables[i].y ) {
+function pacman_vs_erables(positionx,positiony) {
+    for (i=0 ; i<data_erables.length ; i++){
+        if(positionx == data_erables[i].x && positiony == data_erables[i].y ) {
+            var erable_detruit=$('.erable[data-x="'+positionx+'"][data-y="'+positiony+'"]');
+            erable_detruit.animate({
+                top: "-40px",
+                left: "260px",
+                opacity: 0.5
+            },2000);
 
-            $('.erable[data-x="'+pos_pacman.x+'"][data-y="'+pos_pacman.y+'"]').remove();
+            //erable_detruit.remove();
             data_erables.splice(i,1);
             destroyed++;
-
             score();
-            son(point);
-
+            son_point();
             if (destroyed == NOMBRE_ERABLES1){
-                ion.sound.stop("mario_theme");
-                $('#gamebox').fadeOut("slow");
-                ion.sound.play("level_win");
+                end_game();
             }
         }
     }
@@ -220,16 +222,64 @@ function score(){
     $('<p>'+destroyed+'</p>').appendTo($(stats));
     stats.find('p').fadeIn(500);
 }
-function son(text){
-    switch (text)
-        case "point":  switch (destroyed % 2) {
-            case 0: ion.sound.play("point");
+function son_point(){
+    switch (destroyed % 2) {
+        case 0: ion.sound.play("point");
+            break;
+        case 1: ion.sound.play("small_item");
+            break;
+    }
+}
+function end_game(){
+    ion.sound.stop("mario_theme");
+    var erables = $('.erable');
+    for (i=0 ; i<NOMBRE_ERABLES1; i++){
+        switch (i % 4 ) {
+            case 0: erables.eq(i).animate({
+                opacity: 0,
+                top : '-300px',
+                left : '-300px'
+            },2000);
                 break;
-            case 1: ion.sound.play("small_item");
+            case 1: erables.eq(i).animate({
+                opacity: 0,
+                top : '+300px',
+                left : '+300px'
+            },2000);
                 break;
-            case "text" :
+            case 2: erables.eq(i).animate({
+                opacity: 0,
+                top : '-300px',
+                left : '+600px'
+            },2000);
+                break;
+            case 3: erables.eq(i).animate({
+                opacity: 0,
+                top : '+300px',
+                left : '-600px'
+            },2000);
+                break;
         }
+    }
+    $('#gamebox').fadeOut(4000);
+    ion.sound.play("level_win");
+    clearTimeout(t); //Timeout désactivé
+    highscore();
+}
+function highscore(){
+    var highscore = localStorage.high_score;
+    var highscoreminutes = highscore.substr(0, highscore.indexOf("m"));
+    var highscoreseconds = highscore.substr(highscore.indexOf(":")+1,highscore.indexOf("s")-3);
+    if (m<highscoreminutes) {
+        localStorage.high_score = m +"m:" +s + "s" ;
+    } else if (m==highscoreminutes && s<highscoreseconds){
+        localStorage.high_score = m +"m:" +s + "s" ;
+    }
 
+    console.log("le record est de " +highscore);
+    console.log("le record en m est de " +highscoreminutes);
+    console.log("le record est de " + highscoreseconds);
+    $('#black_box3 p').text("High Score : " +localStorage.high_score);
 }
 
 function init_son (){
@@ -264,13 +314,14 @@ function init_son (){
 function startTime() {
     s++;
     if (s==60){s=0; m+=1}
-    $('#black_box2').find('p').text("Timer : " + m + ":" + s);
-    var t = setTimeout(startTime, 1000);
+    $('#black_box2').find('p').text("Timer : " + m + "m:" + s +"s");
+    t = setTimeout(startTime, 1000);
 }
 
 // Le code débute ici !!!
 
 init_son();startTime();
+$('#black_box3 p').text("High Score : " +localStorage.high_score);
 inittableau(map_niveau1);
 var div_info = $("#r1c1");
 var largeur_div = div_info.width();
@@ -285,5 +336,4 @@ document.onkeydown = Mouvement;
 $("label").click(function(){
     $(this).slideUp();
 });
-
 
